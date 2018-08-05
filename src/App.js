50af9335deb1c6bd43c8d57ce3e85a98473a9722
Tim Fischer <emailtimfischer@gmail.com>
2018-08-05 15:19:16 -0600

    Import provider to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Contacts from './components/Contacts';
 import Header from './components/Header';
+import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
