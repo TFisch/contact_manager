be80b1677907852092857cecf9a926d167f646db
Tim Fischer <emailtimfischer@gmail.com>
2018-08-04 20:04:22 -0600

    Push in entire contact instead of individual props

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,42 +2,35 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'John Doe',
-          email: 'jdoe@gmail.com',
-          phone: '666-666-6666'
-        },
-        {
-          id: 2,
-          name: 'Karen Smith',
-          email: 'KSmith@gmail.com',
-          phone: '555-555-5555'
-        },
-        {
-          id: 3,
-          name: 'Henry Johnson',
-          email: 'HJohnson@gmail.com',
-          phone: '666-346-2366'
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '666-666-6666'
+      },
+      {
+        id: 2,
+        name: 'Karen Smith',
+        email: 'KSmith@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'HJohnson@gmail.com',
+        phone: '666-346-2366'
+      }
+    ]
+  };
+
   render() {
     const { contacts } = this.state;
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
