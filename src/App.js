9a03bc4987881af86ad818715cc649b4da6a86ed
Tim Fischer <emailtimfischer@gmail.com>
2018-08-04 19:22:01 -0600

    Add bootstrap properties to nav-bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
-import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = props => {
   const { branding } = props;
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+      </div>
+    </nav>
   );
 };
 
 Header.defaultProps = {
   branding: 'My App'
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;
