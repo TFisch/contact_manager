37a01f13c08fc84fe71793a9cdee26b718e24177
Tim Fischer <emailtimfischer@gmail.com>
2018-08-04 18:46:19 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact
+          name="Karen Smith"
+          email="kdog@gmail.com"
+          phone="555-555-6669"
+        />
+      </div>
+    );
+  }
+}
+export default App;
