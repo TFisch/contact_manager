08ed7a0e101bb2b574c53efdb54b401f5c9bb8d8
Tim Fischer <emailtimfischer@gmail.com>
2018-08-04 18:54:43 -0600

    Import propTypes on branding

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
         <Header branding="Contact Manager" />
         <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
         <Contact
