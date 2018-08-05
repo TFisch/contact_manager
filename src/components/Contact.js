152fa6b465fcc76487a211c43445744e9de2dcc1
Tim Fischer <emailtimfischer@gmail.com>
2018-08-04 20:30:34 -0600

    Change contacts div to react fragment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  onShowClick = e => {
+    console.log('hello');
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i className="fas fa-plus" />
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
         </h4>
         <ul className="list-group">
           <li className="list-group-item">Email:{email}</li>
