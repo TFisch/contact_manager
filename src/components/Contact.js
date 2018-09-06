9808ee0d0d64fef8cd110c138c7bf22a0f478efc
Tim Fischer <emailtimfischer@gmail.com>
2018-09-05 21:01:28 -0600

    Add nav styling on header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class Contact extends Component {
   render() {
@@ -20,8 +21,6 @@ Contact.propTypes = {
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired
-
-
 }
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Header = (props) => {
+  const { branding } = props;
   return (
-    <div>
-      <h1>{props.branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">{branding}</a>
+      </div>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <a href="/" className="nav-link">HOME</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
