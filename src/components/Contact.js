4340ca33b35ad8108d85d401ad0425238ebe2d82
Tim Fischer <emailtimfischer@gmail.com>
2018-09-05 20:51:21 -0600

    Add propTypes to Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,4 +16,12 @@ class Contact extends Component {
   }
 }
 
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+
+
+}
+
 export default Contact;
