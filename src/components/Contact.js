9ae9ae34e27a933e86cbfebacd1e0c3ea92c9160
Tim Fischer <emailtimfischer@gmail.com>
2018-09-05 21:42:06 -0600

    Toggle showContactInfo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 class Contact extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showContactInfo: true
+    }
+  }
 
   onShowClick = () => {
-    console.log('Hello World')
+    let showContactInfo = !this.state.showContactInfo
+    this.setState({ showContactInfo })
   }
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>{name}
           <i onClick={this.onShowClick} className="fas fa-sort-down" />
         </h4>
-        <ul className="list-group">
+        {showContactInfo ? (<ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item ">Phone: {phone}</li>
-        </ul>
+        </ul>) : null}
+
       </div>
 
     )
