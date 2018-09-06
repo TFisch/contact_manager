bcb01f449c209ef8a2b0c3d8176cd389b68c806b
Tim Fischer <emailtimfischer@gmail.com>
2018-09-05 21:33:26 -0600

    Add showContact info to state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 class Contact extends Component {
+
+  onShowClick = () => {
+    console.log('Hello World')
+  }
+
   render() {
-    const { name, email, phone } = this.contact.props;
+    const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>{name}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item ">Phone: {phone}</li>
@@ -18,7 +25,7 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  Contact: Contact.object.isRequired,
+  Contact: PropTypes.object.isRequired,
 }
 
 export default Contact;
