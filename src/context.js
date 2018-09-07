fc91daf3563f4e86fbbf7c445fa175c6ae878127
Tim Fischer <emailtimfischer@gmail.com>
2018-09-06 21:43:52 -0600

    Add Contact action

--- src/context.js
+++ src/context.js
@@ -7,46 +7,52 @@ const reducer = (state, action) => {
     case 'DELETE_CONTACT':
       return {
         ...state,
-        contacts: state.contacts.filter(
-          contact => contact.id !== action.payload
-        )
+        contacts: state.contacts.filter(contact => contact.id !== action.payload)
       };
+    case 'ADD_CONTACT':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      };
+
     default:
       return state;
   }
-};
+}
 
 export class Provider extends Component {
   state = {
     contacts: [
       {
-        id: 1,
         name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '666-666-6666'
+        email: 'JDoe@gmail.com',
+        phone: '666 - 666 - 6666',
+        id: 1
       },
       {
-        id: 2,
         name: 'Karen Smith',
         email: 'KSmith@gmail.com',
-        phone: '555-555-5555'
+        phone: '333 - 333 - 3333',
+        id: 2
       },
       {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'HJohnson@gmail.com',
-        phone: '666-346-2366'
+        name: 'Henry Lee',
+        email: 'HLee@gmail.com',
+        phone: '111 - 222 - 6666',
+        id: 3
       }
     ],
+
     dispatch: action => this.setState(state => reducer(state, action))
-  };
+
+  }
 
   render() {
     return (
       <Context.Provider value={this.state}>
         {this.props.children}
       </Context.Provider>
-    );
+    )
   }
 }
 
