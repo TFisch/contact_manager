e2e69a057023abee8645d8632f09189d8df9ce91
Tim Fischer <emailtimfischer@gmail.com>
2018-09-06 21:43:29 -0600

    Set Router paths

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import Contacts from './components/Contacts';
-import { Header } from './components/Header';
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
 import { Provider } from './context';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AddContact from './components/contacts/AddContact';
 
 class App extends Component {
+
   render() {
     return (
-
-      <div className="App">
-        <Header branding="contact manager" />
-        <div className="container">
-          <Contacts />
-        </div>
-      </div>
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="contact manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about/" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
 
     );
   }
