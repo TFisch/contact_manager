8568e4a11b50030781332cfa4184d027ea1f3969
Tim Fischer <emailtimfischer@gmail.com>
2018-09-06 21:45:09 -0600

    Create About page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = (props) => {
+  return (
+    <div>
+      <h1 className="display-4">Manage Contacts</h1>
+      <p className="lead">Simple app to manage contacts</p>
+      <p className="text-seconda y">1.0.0</p>
+    </div>
+  )
+}
+
+export default About;
